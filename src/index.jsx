cc057c209c86ed29e01c982f789c529421e682f8
flouheforst <flouheforst@gmail.com>
2021-01-27 22:40:28 +0300

    start jsx app file

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <>
         <div>
-            <h1>Opaaa</h1>
+            <h1>Client-app</h1>
         </div>
     </>,
-    document.getElementById("root")
+    document.getElementById('root'),
 );
